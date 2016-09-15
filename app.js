0412dfde27bf167fe70d997858dccb56dada375e
gandha9394 <gandha9394@gmail.com>
2016-09-15 21:07:20 +0530

    Restructuring

--- app.js
+++ app.js
@@ -7,14 +7,11 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var campaigns = require('./routes/campaigns');
-var app = express();
-var expressHbs = require('express3-handlebars');
 
-app.engine('hbs', expressHbs({extname:'html', defaultLayout:'main.html'}));
+var app = express();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
@@ -25,12 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*app.use('/', routes);
+app.use('/', routes);
 app.use('/users', users);
-app.use('/campaigns', campaigns);*/
-app.get('/',function(req,res){
-  res.render('index',{title:"AAA"})
-});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -44,11 +38,11 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    //res.status(err.status || 500);
-    //res.render('error', {
-    //  message: err.message,
-    //  error: err
-    //});
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
   });
 }
 
@@ -56,10 +50,10 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
- /* res.render('error', {
+  res.render('error', {
     message: err.message,
     error: {}
-  });*/
+  });
 });
 
 
--- server/server.js
+++
@@ -1,8 +0,0 @@
-var express =require('express');
-var app = express();
-app.get('/index.html', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(3000, function () {
-  console.log('listening on port 3000!');
-});
