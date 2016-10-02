09cd2fce51ea9641d8034fed15d18e50a609fca3
i_chan93 <chandan.p@keystride.com>
2016-10-02 17:22:20 +0530

    Form submission and validation properties added. Plugins integrated for date,time and file upload

--- public/js/customize.js
+++ public/js/customize.js
@@ -21,6 +21,39 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
+//Login form Validation
+$("#login-form").validate({
+  	rules: {
+      // reg_username: "required",
+  	  // reg_password: "required",
+    },
+  	//errorClass: "form-invalid"
+  });
+
+	// Form Submission
+  $("#login-form").submit(function() {
+  	remove_loading($(this));
+  });
+
+  //Sign up form Validation
+    $("#register-form").validate({
+    	rules: {
+        // reg_username: "required",
+    	  // reg_password: {
+    		// 	required: true,
+    		// 	minlength: 8
+    		// },
+    		// reg_email: {
+    	  //   required: true,
+    		// 	email: true
+    		// },
+      },
+  	  //errorClass: "form-invalid",
+    });
+    // Form Submission
+    $("#register-form").submit(function() {
+    	remove_loading($(this));
+    });
 
 $(function(){
     //Show start campaign section when clicked on Create a campaign button
@@ -39,77 +72,138 @@ $(function(){
 $("#done").on("click", function() {
     $("body").scrollTop(10);
 });
-
-// Sign up form Validation
-   /* $('#register-form').validate({
-            errorPlacement: function(error, element) {
-                error.insertBefore(element);
-            },
-            rules: {
-                reg_username: {
-
-                },
-                reg_email: {
-                    required: true,
-                    email: true
-                },
-                reg_mobile: {
-                    number: true,
-                    maxlength: 10,
-                    minlength: 10
-                },
-            },
-            messages: {
-                reg_email: {
-                    required: "Please enter email address",
-                    email: "Please enter a valid email address"
-                },
-                reg_mobile: "Please enter valid mobile number"
-            },
-            submitHandler: function(form) {
-                $('#register-form')[0].reset();
-            }
-});*/
-
-// Login form Validation
-  /*  $('#login-form').validate({
-            errorPlacement: function(error, element) {
-                error.insertBefore(element);
-            },
-            rules: {
-                reg_username: {
-                    required: true,
-                },
-                reg_password: {
-                    required: true,
-                    minlength: 8,
-                }
-            },
-            messages: {
-                reg_username: {
-                    required: "Please enter username",
-                    // email: "Please enter a valid email address"
-                },
-                reg_password: "Password should be minimum 8 characters long"
-            },
-            submitHandler: function(form) {
-                $('#login-form')[0].reset();
-            }
-});*/
-
-/*$.validator.methods.email = function( value, element ) {
+$.validator.methods.email = function( value, element ) {
     return this.optional( element ) || /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/igm.test( value );
 }
-*/
-// !function(e){var t=function(t,n){this.$element=e(t),this.type=this.$element.data("uploadtype")||(this.$element.find(".thumbnail").length>0?"image":"file"),this.$input=this.$element.find(":file");if(this.$input.length===0)return;this.name=this.$input.attr("name")||n.name,this.$hidden=this.$element.find('input[type=hidden][name="'+this.name+'"]'),this.$hidden.length===0&&(this.$hidden=e('<input type="hidden" />'),this.$element.prepend(this.$hidden)),this.$preview=this.$element.find(".fileupload-preview");var r=this.$preview.css("height");this.$preview.css("display")!="inline"&&r!="0px"&&r!="none"&&this.$preview.css("line-height",r),this.original={exists:this.$element.hasClass("fileupload-exists"),preview:this.$preview.html(),hiddenVal:this.$hidden.val()},this.$remove=this.$element.find('[data-dismiss="fileupload"]'),this.$element.find('[data-trigger="fileupload"]').on("click.fileupload",e.proxy(this.trigger,this)),this.listen()};t.prototype={listen:function(){this.$input.on("change.fileupload",e.proxy(this.change,this)),e(this.$input[0].form).on("reset.fileupload",e.proxy(this.reset,this)),this.$remove&&this.$remove.on("click.fileupload",e.proxy(this.clear,this))},change:function(e,t){if(t==="clear")return;var n=e.target.files!==undefined?e.target.files[0]:e.target.value?{name:e.target.value.replace(/^.+\\/,"")}:null;if(!n){this.clear();return}this.$hidden.val(""),this.$hidden.attr("name",""),this.$input.attr("name",this.name);if(this.type==="image"&&this.$preview.length>0&&(typeof n.type!="undefined"?n.type.match("image.*"):n.name.match(/\.(gif|png|jpe?g)$/i))&&typeof FileReader!="undefined"){var r=new FileReader,i=this.$preview,s=this.$element;r.onload=function(e){i.html('<img src="'+e.target.result+'" '+(i.css("max-height")!="none"?'style="max-height: '+i.css("max-height")+';"':"")+" />"),s.addClass("fileupload-exists").removeClass("fileupload-new")},r.readAsDataURL(n)}else this.$preview.text(n.name),this.$element.addClass("fileupload-exists").removeClass("fileupload-new")},clear:function(e){this.$hidden.val(""),this.$hidden.attr("name",this.name),this.$input.attr("name","");if(navigator.userAgent.match(/msie/i)){var t=this.$input.clone(!0);this.$input.after(t),this.$input.remove(),this.$input=t}else this.$input.val("");this.$preview.html(""),this.$element.addClass("fileupload-new").removeClass("fileupload-exists"),e&&(this.$input.trigger("change",["clear"]),e.preventDefault())},reset:function(e){this.clear(),this.$hidden.val(this.original.hiddenVal),this.$preview.html(this.original.preview),this.original.exists?this.$element.addClass("fileupload-exists").removeClass("fileupload-new"):this.$element.addClass("fileupload-new").removeClass("fileupload-exists")},trigger:function(e){this.$input.trigger("click"),e.preventDefault()}},e.fn.fileupload=function(n){return this.each(function(){var r=e(this),i=r.data("fileupload");i||r.data("fileupload",i=new t(this,n)),typeof n=="string"&&i[n]()})},e.fn.fileupload.Constructor=t,e(document).on("click.fileupload.data-api",'[data-provides="fileupload"]',function(t){var n=e(this);if(n.data("fileupload"))return;n.fileupload(n.data());var r=e(t.target).closest('[data-dismiss="fileupload"],[data-trigger="fileupload"]');r.length>0&&(r.trigger("click.fileupload"),t.preventDefault())})}(window.jQuery)
 
-$(function() {
-    $("#fileuploader").uploadFile({
-            url:"YOUR_FILE_UPLOAD_URL",
-            fileName:"myfile"
-        });
+
+//DateTimePicker functions
+$(function () {
+  $('#datetimepicker1').datetimepicker({
+    format: 'DD/MM/YYYY'    //excludes time
+  });
+
+  $('#datetimepicker2').datetimepicker({
+      format: 'DD/MM/YYYY',     //excludes time
+      useCurrent: false //Important! See issue #1075
+  });
+  //linked date picker
+  $("#datetimepicker1").on("dp.change", function (e) {
+      $('#datetimepicker2').data("DateTimePicker").minDate(e.date);
+  });
+  $("#datetimepicker2").on("dp.change", function (e) {
+      $('#datetimepicker1').data("DateTimePicker").maxDate(e.date);
+  });
+  $('#timepicker1').datetimepicker({
+    format: 'LT'
+  });
 });
-// var uploadObj = $("#uploadDivId").uploadFile(options);
-// uploadObj.startUpload();
-// uploadObj.stopUpload();
-// uploadObj.cancelAll();
+
+//Campaign name validation
+$('#campaign-name').on("keyup",function () {
+  if($('#campaign-name-form #campaign-name').val().length > 3){
+      $('.tick1').show();
+  }
+  else if ($('#campaign-name-form #campaign-name').val().length <= 3){
+      $('.tick1').hide();
+  }
+});
+
+$('#campaign-name-form').validate({
+          errorPlacement: function(error, element) {
+              error.insertBefore(element);
+          },
+          			rules: {
+          				campaign_name: {
+                      required: true
+          				}
+          			},
+          			messages: {
+          				campaign_name: "Please enter valid name"
+          			},
+          			submitHandler: function(form) {
+          	            $.post(
+          	                //$templateDir + "/snippets/form-submit.php",  //Create a snippet tot include all submit actions - Gandharva
+          	                //$('#campaign-name-form').serialize(),
+          	            );
+          	            //$('.landing-enquiry-form')[0].reset();
+          	        }
+          		});
+
+//File Upload functions
+$(function () {
+    $('#fileupload').fileupload({
+        dataType: 'csv',
+        done: function (e, data) {
+            $.each(data.result.files, function (index, file) {
+                $('<p/>').text(file.name).appendTo(document.body);
+            });
+        }
+    });
+});
+
+//Contact list upload form
+$('#contact-list-upload').validate({
+          errorPlacement: function(error, element) {
+              error.insertBefore(element);
+          },
+          			rules: {
+
+          			},
+          			messages: {
+          				files: "Please enter a valid file"
+          			},
+          			submitHandler: function(form) {
+          	            $.post(
+          	                //$templateDir + "/snippets/form-submit.php",  //Create a snippet tot include all submit actions - Gandharva
+          	                //$('#campaign-name-form').serialize(),
+          	            );
+          	            //$('#campaign-name-form')[0].reset();
+          	        }
+          		});
+
+
+  //Voice upload form
+              $('#voice-upload').validate({
+                        errorPlacement: function(error, element) {
+                            error.insertBefore(element);
+                        },
+                        			rules: {
+
+                        			},
+                        			messages: {
+                        				files: "Please enter a valid file"
+                        			},
+                        			submitHandler: function(form) {
+                        	            $.post(
+                        	                //$templateDir + "/snippets/form-submit.php",  //Create a snippet tot include all submit actions - Gandharva
+                        	                //$('#voice-upload').serialize(),
+                        	            );
+                        	            //$('#voice-upload')[0].reset();
+                        	        }
+                        		});
+
+//date-time-form
+$('#date-time-form').validate({
+          errorPlacement: function(error, element) {
+              error.insertBefore(element);
+          },
+          			rules: {
+
+          			},
+          			messages: {
+
+          			},
+          			submitHandler: function(form) {
+          	            $.post(
+          	                //$templateDir + "/snippets/form-submit.php",  //Create a snippet tot include all submit actions - Gandharva
+          	                //$('#date-time-form').serialize(),
+          	            );
+          	            //$('#date-time-form')[0].reset();
+          	        }
+          		});
+
+//Done submit action
+$('#done').click(function(){
+
+});
